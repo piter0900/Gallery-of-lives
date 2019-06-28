7537481ca06e6049f0fe6081f3ab562fc175e57d
piter0900 <piter0900@gmail.com>
2019-06-28 16:08:46 -0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroDetailComponent }      from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: DashboardComponent },
+  { path: 'see-lives', component: HeroesComponent },
+  { path: 'share-life', component: HeroDetailComponent }
 ];
 
 @NgModule({
