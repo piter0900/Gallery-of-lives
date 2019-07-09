409286671d498f0415ed8f7bc3107fb933814e76
piter0900 <piter0900@gmail.com>
2019-07-09 11:18:14 -0700

    4 files modified

--- src/app/vid-recording/vid-recording.component.ts
+++ src/app/vid-recording/vid-recording.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vid-recording.component.css']
 })
 export class VidRecordingComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
-
+  top_line = ["Day in the life","The good,the bad and the ugly","My journey, my learnings","Anyone can become"]; 
 }
